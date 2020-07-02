197d23da43840d2be3d7c1507b439ba5b42563ee
Nguyen Thanh <ndthanh20@gmail.com>
2020-07-03 01:14:03 +0700

    Add the letters into the animals and update sound effect when choosing the letter

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import "phaser";
-import {GameScene} from "./gameScene"
+import {Scene1} from "./scene1"
+import { Scene2 } from "./scene2";
+import { Scene3 } from "./scene3";
+import {Scene4} from "./scene4";
 const config: Phaser.Types.Core.GameConfig = {
   title: "Choose Letter",
-  width: 1600,
-  height: 900,
-  parent: "game",
-  scene: [GameScene],
+  type: Phaser.AUTO,
+  scale: {
+          mode: Phaser.Scale.FIT,
+          width: window.innerWidth,
+          height: window.innerHeight,
+          parent: "game",
+          autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: [Scene1,Scene2,Scene3,Scene4],
   backgroundColor: "#18216D"
 };
 export class ChooseLetterGame extends Phaser.Game {
---
+++ src/scene3.ts
@@ -0,0 +1,111 @@
+import "phaser";
+import { Bear } from "../object/bear";
+export class Scene3 extends Phaser.Scene {
+    //private bear: Bear;
+    audio;
+    constructor() {
+        super({
+            key: "Scene3"
+        });
+    }
+    init(/*params: any*/): void {
+
+    }
+    preload(): void {
+        this.load.image("sheep", "assets/sheep-01.png");
+        this.load.image("sheep", "assets/sheep-01.png");
+        this.load.image("sheep", "assetssheep-01.png");
+        this.load.spritesheet('bear', "assets/bear3-01.png", { frameWidth: 311, frameHeight: 450 });
+        this.load.audio('audio', ["assets/translate_tts.ogg", "translate_tts.mp3"]);
+        this.load.audio('try_again', ["assets/try_again.ogg", "try_again.mp3"]);
+    }
+    create(): void {
+        var myImage = this.add.image(300, 400, 'sheep').setInteractive({ cursor: 'pointer' });
+        myImage.setScale(0.08);
+
+        var myImage2 = this.add.image(750, 400, 'sheep').setInteractive({ cursor: 'pointer' });
+        myImage2.setScale(0.08);
+
+        var myImage3 = this.add.image(1200, 400, 'sheep').setInteractive({ cursor: 'pointer' });
+        myImage3.setScale(0.08);
+
+
+        //var bear = this.add.image(1500,700,'bear');
+
+        this.anims.create({
+            key: 'talk',
+            frames: this.anims.generateFrameNames('bear', { start: 0, end: 1 }),
+            frameRate: 2,
+            repeat: 1,
+        });
+
+        var bear1 = this.add.sprite(1500, 700, 'bear').play('talk').setInteractive({ cursor: 'pointer' });
+
+        this.audio = this.sound.add("audio");
+
+        this.audio.play();
+
+        bear1.on('pointerdown', this.startClick, this);
+
+        bear1.on('pointerdown', function () {
+            bear1.play('talk');
+        }, this);
+
+        myImage.on('pointerdown', function () {
+            this.sound.add("try_again").play();
+            var tween = this.tweens.add({
+                targets: myImage,
+                scale: 0.1,
+                ease: 'Power1',
+                duration: 800,
+                yoyo: true,
+                repeat: 0
+            });
+            bear1.play('talk');
+        }, this);
+        myImage2.on('pointerdown', function () {
+            var tween = this.tweens.add({
+                targets: myImage2,
+                scale: 0.1,
+                ease: 'Power1',
+                duration: 800,
+                yoyo: false,
+                repeat: 0
+            });
+            bear1.play('talk');
+            this.finish();
+        }, this);
+        myImage3.on('pointerdown', function () {
+            this.sound.add("try_again").play();
+            var tween = this.tweens.add({
+                targets: myImage3,
+                scale: 0.1,
+                ease: 'Power1',
+                duration: 800,
+                yoyo: true,
+                repeat: 0
+            });
+            bear1.play('talk');
+        }, this)
+        
+    }
+    private startClick(pointer, targets) {
+
+        this.input.off('pointerdown', this.startClick, this);
+        this.audio.play();
+    }
+    private finish(){
+        this.time.addEvent({
+            delay:2500,
+            callback: ()=>{
+                //this.tweens.killAll();
+                this.scene.start('Scene4');
+            }
+        })
+    }
+    private tryAgain() {
+
+    }
+    update(): void {
+    }
+}
